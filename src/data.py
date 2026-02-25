71d371a45610a34792f6297b8411062138f2747c
tre3iii <lilshed3@gmail.com>
2026-02-24 22:43:50 -0800

    improve init scripts

--- src/data.py
+++ src/data.py
@@ -2,13 +2,13 @@ from datasets import load_dataset, get_dataset_split_names
 import numpy as np
 import torch
 
+# if no split is specified, a dict is returned
 dataset_base = load_dataset("cnn_dailymail", "3.0.0")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ds = dataset_base.with_format("torch", device=device)
 
-print(get_dataset_split_names("cnn_dailymail", "3.0.0"))
-
-# if no split is specified, a dict is returned
+print(ds[0])
 
 """
 training_data = dataset["train"]
