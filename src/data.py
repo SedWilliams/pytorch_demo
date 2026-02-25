35ab5c79b67beee3044ed391d8853d6832e1940b
tre3iii <lilshed3@gmail.com>
2026-02-24 22:49:28 -0800

    remove __init__.py (its not needed\)
    "
    "

--- src/data.py
+++ src/data.py
@@ -1,11 +1,13 @@
-from datasets import load_dataset, get_dataset_split_names
+from datasets import load_dataset, get_dataset_split_names, Dataset
 import numpy as np
 import torch
 
-# if no split is specified, a dict is returned
-dataset_base = load_dataset("cnn_dailymail", "3.0.0")
-
+# check if cuda is available and set the device accordingly
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# if no split is specified, a dict is returned
+dataset_base = load_dataset("cnn_dailymail", "3.0.0", split="train[:1%]")
+
 ds = dataset_base.with_format("torch", device=device)
 
 print(ds[0])
