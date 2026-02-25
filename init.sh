00cdfd35c40641bab1d732f053c1f9a427257092
tre3iii <lilshed3@gmail.com>
2026-02-24 22:44:43 -0800

    improve init scripts

--- init.sh
+++ init.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
+# this script makes sure a virtual environment is created and dependencies are installed
+
 CURRENT_PATH=$(pwd)
 
-if [[ -d "$CURRENT_PATH/venv" ]]; then
-    echo "Virtual environment already exists. Activating it now."
-    source venv/bin/activate
-    echo "Virtual environment activated."
-    echo "Updating dependencies..."
-    pip install -r requirements.txt --quiet
-else
+# check if venv exists, if not then create it
+if [[ ! -d "$CURRENT_PATH/venv" ]]; then
     echo "No virtual environment found. Creating a new one now."
     python3 -m venv venv
-    echo "Virtual environment created. Now sourcing..."
-    source venv/bin/activate
-    echo "Virtual environment activated. Installing dependencies..."
-    pip install -r requirements.txt
+    echo "Virtual environment created. Please source with source venv/bin/activate and run this script again."
+    exit 0
 fi
+
+# check to make sure the virtual environment is activated before installing dependencies
+if [[ -z $VIRTUAL_ENV ]]; then
+    echo "Need to source the virtual environment with: source venv/bin/activate"
+    exit 1
+fi
+
+# when the previous checks are passed, then we can install dependencies
+echo "Updating dependencies..."
+chmod +x install_deps.sh
+./install_deps.sh
 
 cat << EOF
  
 Virtual environment created and dependencies installed.
-To run demo enter the following:
-python -m main
+
+Run the code with: python -m src.main
 EOF
 
---
+++ install_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# this script installs pip dependencies
+
+pip install -r requirements.txt
+
+echo "
+All dependencies have been installed successfully.
+"
