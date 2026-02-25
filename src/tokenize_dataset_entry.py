95286270b298ab88c00d8b154fa799fff0e43da7
tre3iii <lilshed3@gmail.com>
2026-02-24 23:05:34 -0800

    data.py -

---
+++ src/tokenize_dataset_entry.py
@@ -0,0 +1,49 @@
+
+"""
+This script loads one of our datasets and tokenizes the first entry in the dataset.
+"""
+
+from datasets import load_dataset, get_dataset_split_names, Dataset
+import torch
+import nltk
+
+# check if cuda is available and set the device accordingly
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# if no split is specified, a dict is returned
+dataset_base = load_dataset("cnn_dailymail", "3.0.0", split="train")
+
+# download the punkt tokenizer models
+nltk.download("punkt")
+
+### parse sentences -> tokenize
+sentences = nltk.sent_tokenize(dataset_base[0]["article"])
+print("\r\n")
+print(f"Sentences: {sentences}")
+print("\r\n")
+
+token_bag = list()
+# iterate over each sentence...
+for sentence in sentences:
+    # for each token in the sentence, add it to the token bag
+    for word in sentence.split():
+        token_bag.append(word)
+
+print("\r\n")
+print(f"Token bag: {token_bag}")
+print("\r\n")
+
+
+#make tensor
+#tensor = dataset_base.with_format("torch", device=device)
+
+#print(tensor[0])
+
+"""
+training_data = dataset["train"]
+testing_data = dataset["test"]
+
+example = training_data[0]
+
+print(f"{example}")
+"""
