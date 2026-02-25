0a8332dbbd1cf6bbcd8aa2268fca5e485cf89bdb
tre3iii <lilshed3@gmail.com>
2026-02-24 23:32:31 -0800

    basic tokenization pipeline

---
+++ tokenize_dataset_entry.py
@@ -0,0 +1,7 @@
+
+[main 9528627] data.py -
+ 3 files changed, 49 insertions(+), 22 deletions(-)
+ delete mode 100644 src/data.py
+ create mode 100644 src/tokenize_dataset_entry.py
+ create mode 100644 tokenize_dataset_entry.py
+
