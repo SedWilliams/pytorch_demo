10da642fd854aba6d2957608ef829f72befbfa5a
tre3iii <lilshed3@gmail.com>
2026-02-28 22:28:39 -0800

    hook up google embedding model

--- tokenize_dataset_entry.py
+++
@@ -1,7 +0,0 @@
-
-[main 9528627] data.py -
- 3 files changed, 49 insertions(+), 22 deletions(-)
- delete mode 100644 src/data.py
- create mode 100644 src/tokenize_dataset_entry.py
- create mode 100644 tokenize_dataset_entry.py
-
